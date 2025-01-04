e03e32d2f7af891ea6fd89a057f1515d7c765819
jerson13 <jersonerazo@gmail.com>
2025-01-04 14:42:49 -0500

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 module.exports = {
     async getAll(req, res, next) { 
         try {
-            const data = await User.getAll();
+            const data = await user.getAll();
             console.log(`Usuarios: ${data}`);
             return res.status(200).json(data);
         }
